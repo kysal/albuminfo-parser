1a4d902708bb8b5e4b1bb020565a4263dda13f10
Kyle <kylesalisbury2@gmail.com>
2025-11-29 18:03:20 +0000

    refactor album info parser

--- tidal-metadata-fix/__init__.py
+++ tidal-metadata-fix/__init__.py
@@ -1,63 +1,24 @@
-import re
-import sys
-import os
 from pprint import pprint
+from album_info_parser import *
 
-def get_filepath() -> str:
+def get_directory() -> str:
   if len(sys.argv) < 2:
-    print("Error: No file provided.")
-    print("Usage: python __init__.py <file>")
+    print("Error: No directory provided.")
+    print("Usage: python __init__.py <album-directory>")
     sys.exit(1)
   
-  file_path = sys.argv[1]
+  file_dir = sys.argv[1]
   
-  if not os.path.isfile(file_path):
-    print(f"Error: '{file_path}' is not a valid file")
+  if not os.path.isdir(file_dir):
+    print(f"Error: '{file_dir}' is not a valid file directory")
     sys.exit(1)
   
-  return file_path  
-
-def parse_album_info(raw_album_info: str):
-  album_info = {
-    "Discs": []
-  }
-  
-  data_points = raw_album_info.splitlines()
-  header_pattern = re.compile(r'\[(.*?)\]\s+(.+)')
-  
-  line_index = 0;
-  
-  # header details
-  while data_points[line_index] != "":
-    header_match = header_pattern.match(data_points[line_index])
-    key, value = header_match.groups()
-    album_info[key] = value.strip()
-    line_index += 1
-    
-  line_index += 2
-  
-  # disc track details
-  track_pattern = re.compile(r'\[(\d+)\]\s+(.+)')
-  current_disc = []
-  while line_index < len(data_points):
-    if data_points[line_index][0] == "=":
-      album_info["Discs"].append(current_disc)
-      current_disc = []
-      line_index += 1
-      continue
-    
-    track_match = track_pattern.match(data_points[line_index])
-    track_num, track_title = track_match.groups()
-    current_disc.append({"TrackNumber": int(track_num), "Title": track_title})
-    line_index += 1
-    
-  album_info["Discs"].append(current_disc)
-
-  return album_info
+  return file_dir  
 
 if __name__ == "__main__":
-  file_path = get_filepath()
-  with open(file_path, "r") as file:
+  dir = get_directory()
+  print(dir)
+  with open(dir + "/AlbumInfo.txt", "r") as file:
     raw_album_info = file.read()
     album_info = parse_album_info(raw_album_info)
     pprint(album_info)
---
+++ tidal-metadata-fix/album_info_parser.py
@@ -0,0 +1,55 @@
+import re
+import sys
+import os
+
+def get_filepath() -> str:
+  if len(sys.argv) < 2:
+    print("Error: No file provided.")
+    print("Usage: python __init__.py <file>")
+    sys.exit(1)
+  
+  file_path = sys.argv[1]
+  
+  if not os.path.isfile(file_path):
+    print(f"Error: '{file_path}' is not a valid file")
+    sys.exit(1)
+  
+  return file_path  
+
+def parse_album_info(raw_album_info: str):
+  album_info = {
+    "Discs": []
+  }
+  
+  data_points = raw_album_info.splitlines()
+  header_pattern = re.compile(r'\[(.*?)\]\s+(.+)')
+  
+  line_index = 0;
+  
+  # header details
+  while data_points[line_index] != "":
+    header_match = header_pattern.match(data_points[line_index])
+    key, value = header_match.groups()
+    album_info[key] = value.strip()
+    line_index += 1
+    
+  line_index += 2
+  
+  # disc track details
+  track_pattern = re.compile(r'\[(\d+)\]\s+(.+)')
+  current_disc = []
+  while line_index < len(data_points):
+    if data_points[line_index][0] == "=":
+      album_info["Discs"].append(current_disc)
+      current_disc = []
+      line_index += 1
+      continue
+    
+    track_match = track_pattern.match(data_points[line_index])
+    track_num, track_title = track_match.groups()
+    current_disc.append({"TrackNumber": int(track_num), "Title": track_title})
+    line_index += 1
+    
+  album_info["Discs"].append(current_disc)
+
+  return album_info
